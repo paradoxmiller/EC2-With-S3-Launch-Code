94c269e0fe8a3215483280a6c46de6e60f9496f4
paradoxmiller <33523467+paradoxmiller@users.noreply.github.com>
2017-11-12 13:12:01 -0800

    Update terminateEC2nS3.sh
    Adding Yes/No confirmation to continue script.

--- terminateEC2nS3.sh
+++ terminateEC2nS3.sh
@@ -1,6 +1,15 @@
 #!/bin/bash -e
 echo "This script will terminate the EC2 instance $INSTANCEID and"
 echo "the S3 bucket $bname"
+echo ".... .... .... .... .... .... ...."
+echo "Do you wish to continue?:"
+
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+done
 
 read -p "Press [Enter] key to terminate $INSTANCEID ..."
 aws ec2 terminate-instances --instance-ids $INSTANCEID
